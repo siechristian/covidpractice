9e3c144ea606107b5f166901cbac4557dd1d1ea2
CodenificenT <61930093+siechristian@users.noreply.github.com>
2020-11-14 11:39:45 -0500

    fix countrylist

--- src/Components/CountryList/CountryList.js
+++ src/Components/CountryList/CountryList.js
@@ -1,7 +1,7 @@
 import Country from '../Country/Country';
 import './countryList.css';
 
-const CountryList = ({ stats, countries }) => {
+const CountryList = ({ stats }) => {
 	return (
 		<div className="countryList">{stats.map((country) => <Country key={country.country} stats={country} />)}</div>
 	);
